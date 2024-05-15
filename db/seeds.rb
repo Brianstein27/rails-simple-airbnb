2979c09b671ee88408edaf313fb99a2dca382ea1
Brian Sauls <brianstein27@proton.me>
2024-05-15 17:58:41 +0200

    Add CRUD airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  flat = Flat.new(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    price_per_night: rand(50..120),
+    number_of_guests: rand(1..8)
+  )
+
+  flat.save
+end
